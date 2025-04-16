f98849819456c69d566ed06148f153a6186d8788
İlker SIĞIRCI <sigirci.ilker@gmail.com>
2025-04-16 22:57:19 +0300

    Initial commit

---
+++ src/langgraph_openai_serve/__init__.py
@@ -0,0 +1,6 @@
+"""langgraph-openai-serve package."""
+
+from importlib.metadata import version
+
+# Fetches the version of the package as defined in pyproject.toml
+__version__ = version("langgraph_openai_serve")
